419d001f6a4282e1b5ea9dcd8ba26f16f70c2de8
Khaled Karim <kskarim177@gmail.com>
2023-05-30 20:28:18 +0600

    half of the way

---
+++ Module 6/string_constructor.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s("helasdflo world");
+    // string s("hello world");
+    // string s("hello world", 7);
+    // string a = "hellooooo";
+    // string s(a, 4);
+    string s(26, 'a'); // to print 26 a's
+    cout << s << endl;
+    return 0;
+}
